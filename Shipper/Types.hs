81fb4f1294a3f1ce22adc454d7b1c86e23a087b8
Christian Marie <pingu@anchor.net.au>
2013-11-22 15:21:02 +1100

    Indentation

--- Shipper/Types.hs
+++ Shipper/Types.hs
@@ -1,31 +1,31 @@
-  module Shipper.Types (
-      Event(..),
-      Input(..),
-      Output(..),
-      ShipperConfig(..),
-  )
-  where
-  
-  import qualified Data.ByteString.Char8 as B
-  import Data.Time
-  
-  data Event = Event
-      { message :: B.ByteString
-      , source  :: String
-      , tags    :: [String]
-      , tipe    :: String
-      , time    :: UTCTime
-      } deriving (Show)
-  
-  data Input = FileInput
-      { fTags    :: [String]
-      , fType    :: String
-      , filePath :: FilePath
-      } deriving (Show)
-  
-  data Output = Debug
-  
-  data ShipperConfig = Config
-      { inputs  :: [Input]
-      , outputs :: [Output]
-      }
+module Shipper.Types (
+    Event(..),
+    Input(..),
+    Output(..),
+    ShipperConfig(..),
+)
+where
+
+import qualified Data.ByteString.Char8 as B
+import Data.Time
+
+data Event = Event
+    { message :: B.ByteString
+    , source  :: String
+    , tags    :: [String]
+    , tipe    :: String
+    , time    :: UTCTime
+    } deriving (Show)
+
+data Input = FileInput
+    { fTags    :: [String]
+    , fType    :: String
+    , filePath :: FilePath
+    } deriving (Show)
+
+data Output = Debug
+
+data ShipperConfig = Config
+    { inputs  :: [Input]
+    , outputs :: [Output]
+    }
