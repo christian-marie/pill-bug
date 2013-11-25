3ef2a5c7af5b9bdabaada56ab7eb433e034b79d2
Christian Marie <pingu@anchor.net.au>
2013-11-25 14:51:59 +1100

    This seems nicer

--- Main.hs
+++ Main.hs
@@ -2,16 +2,15 @@ module Main (main) where
 
 import Shipper
 import Shipper.ConfigParser (parseConfig)
-import Control.Monad (unless)
 import System.Environment (getArgs, getProgName)
 import System.Exit (exitFailure)
 
 main :: IO ()
-main = do
-    args <- getArgs 
-    unless (length args == 1) $ do 
+main = start =<< getArgs
+  where
+    start [config_path] = startShipper =<< parseConfig config_path
+    start _ = do
         name <- getProgName
         putStrLn $ "Usage: " ++ name ++ " CONFIG"
         exitFailure
 
-    startShipper =<< parseConfig (head args)
