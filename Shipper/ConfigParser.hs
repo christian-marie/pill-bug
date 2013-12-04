a3c558ee1af24511d3188624be6996dbeb5d7dbd
Christian Marie <pingu@anchor.net.au>
2013-12-05 09:13:16 +1100

    Clean up parser and move to trifecta for user friendly errors.

--- Shipper/ConfigParser.hs
+++ Shipper/ConfigParser.hs
@@ -3,7 +3,7 @@ module Shipper.ConfigParser (
 ) where
 
 import Shipper.Types
-import Text.ParserCombinators.Parsec hiding ((<|>), many)
+import Text.Trifecta
 import Control.Monad
 import Control.Applicative
 import Control.DeepSeq
@@ -27,7 +27,10 @@ import Data.Restricted
 -- }
 
 parseConfig :: FilePath -> IO [ConfigSegment]
-parseConfig f = parseFromFile config f >>= either (error.show) (return.force)
+parseConfig f = do
+    result <- parseFromFile config f 
+    return $ case result of Just cfg -> force cfg
+                            Nothing -> error "Failed to parse config"
 
 -- Create a file input segment, requires only 'path'
 fileInputSegment :: [ExtraInfoPair] -> ConfigSegment
@@ -167,18 +170,18 @@ redisOutputSegment infos = OutputSegment Redis
         Nothing -> error "Redis output expected 'key' to be specified"
 
 -- A config is zero or more segments
-config :: GenParser Char st [ConfigSegment]
+config :: Parser [ConfigSegment]
 config = many segment <* eof
 
 -- Segments are built here, by applying the function parsed from beginSegment
 -- to the directives eaten within that segment. Simple! Sort of.
-segment :: GenParser Char st ConfigSegment
+segment :: Parser ConfigSegment
 segment = ap 
     (spaces *> beginSegment)
     (spaces *> directives <* endSegment <* spaces)
 
 -- We try to see if this is a valid segment by parsing the word before a '{'
-beginSegment :: GenParser Char st ( [ExtraInfoPair] -> ConfigSegment )
+beginSegment :: Parser ([ExtraInfoPair] -> ConfigSegment)
 beginSegment = possibleSegment <* spaces <* char '{' 
   where
     -- Any valid segments need to be defined here, returning function that
@@ -189,64 +192,60 @@ beginSegment = possibleSegment <* spaces <* char '{'
                       <|> zmq4InputSegment   <$ (try $ string "zmq4in")
                       <|> redisOutputSegment <$ string "redis"
 
-endSegment :: GenParser Char st ()
+endSegment :: Parser ()
 endSegment = void $ char '}'
 
-directives :: GenParser Char st [ExtraInfoPair]
+directives :: Parser [ExtraInfoPair]
 directives = many directive
 
 -- We turn a = [b,  c] into ("a", ["b", "c"]), wrapped inside ExtraInfo
-directive :: GenParser Char st ExtraInfoPair
+directive :: Parser ExtraInfoPair
 directive = liftA2 (,)
-    (spaces *> key <* spaces <* char '=')
+    (spaces *> key <* spaces)
     (spaces *> extraInfo <* spaces)
 
 -- A key must not include any magic characters that mean things to values or
 -- segments as they are used as an anchor for values spanning multiple lines.
-key :: GenParser Char st Key
-key = manyTill (noneOf "\r\n}'\"") (try . lookAhead $ equals)
-  where equals = spaces <* char '='
+key :: Parser String
+key = manyTill (noneOf "\r\n}'\"") ( spaces *> char '=')
 
 
 -- A key may have a list or string, that list may have lists, and that list
 -- lists and so ad infinitum. Also maps may recurse.
-extraInfo :: GenParser Char st ExtraInfo
+extraInfo :: Parser ExtraInfo
 extraInfo = spaces *> (extraMap <|> extraList <|> extraString) <* spaces
-
-extraList, extraString, extraMap :: GenParser Char st ExtraInfo
-
--- Strings are just wrapped strings
-extraString = fmap ExtraString anyString
-
--- A list is denoted by [, this is convenient for not backtracking.
-extraList = fmap ExtraList $ 
-    char '[' *> extraInfo `sepBy` separator <* char ']'
-
--- A map is a nested set of directives.
-extraMap = fmap ExtraMap $ 
-    char '{' *> directive `sepBy` separator <* char '}'
-
-anyString :: CharParser st String
-anyString = quotedString '"' <|> quotedString '\'' <|> literalString
--- Literal strings are basically barewords, the only limitation being
--- whitespace at the beginning/end of lines, double quotes and commas.
-literalString :: CharParser st String
-literalString = strip `liftM` (many $ noneOf ",\n\r'\"[]}")
-    <?> "literal string"
-
--- If you need whitespace at the beginning/end of lines, double quotes or
--- commas, you have a quoted string with \" to escape double quotes
-quotedString :: Char -> CharParser st String
-quotedString q = concat `liftM` do
-    quote *> many quotedContents <* quote <?> "quoted string"
   where
-    quote          = char q
-    quotedContents = many1 (quotedText <|> quoteEscape)
-    quotedText     = noneOf ("\\\r\n" ++ [q]) <?> "quoted text"
-    quoteEscape    = char '\\' *> char q <?> "quote escape"
+    -- Strings are just wrapped strings
+    extraString = fmap ExtraString anyString
 
-separator :: GenParser Char st ()
-separator = void $ char ','
+    -- A list is denoted by [, this is convenient for not backtracking.
+    extraList = fmap ExtraList $ 
+        char '[' *> extraInfo `sepBy` separator <* char ']'
+
+    -- A map is a nested set of directives.
+    extraMap = fmap ExtraMap $ 
+        char '{' *> directive `sepBy` separator <* char '}'
+
+    separator = void $ char ','
+
+anyString :: Parser String
+anyString = quotedString '"' <|> quotedString '\'' <|> literalString
+  where
+    -- Literal strings are basically barewords, the only limitation being
+    -- whitespace at the beginning/end of lines, double quotes and commas.
+    literalString = strip `liftM` (many $ noneOf ",\n\r'\"[]}")
+        <?> "literal string"
+
+    -- If you need whitespace at the beginning/end of lines, double quotes or
+    -- commas, you have a quoted string with \" to escape double quotes
+    quotedString q = concat `liftM` do
+        quote *> many quotedContents <* quote <?> "quoted string"
+      where
+        quote          = char q
+        quotedContents = some (quotedText <|> quoteEscape)
+        quotedText     = noneOf ("\\\r\n" ++ [q]) <?> "quoted text"
+        quoteEscape    = char '\\' *> char q <?> "quote escape"
+
 
 lstrip, rstrip, strip:: String -> String
 lstrip s = case s of
